3df63290a323f4d3a6f8d630757110815470046c
Yahor Urbanovich <egorikftp@gmail.com>
2020-05-23 00:03:38 +0300

    Initial sample commit

---
+++ ladyhappy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ladyhappy
+//
+//  Created by Yahor Urbanovich on 5/22/20.
+//  Copyright © 2020 Yahor Urbanovich. All rights reserved.
+//
+
+import UIKit
+ 
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      print("I Love my Gulia :3")
+    }
+
+}
+
